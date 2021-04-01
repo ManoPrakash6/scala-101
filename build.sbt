c09e7e9c48bb80a90c693e45b2d46479c5fb4599
P <mp24@LAMU02ZJ407LVCF.uhc.com>
2021-04-01 22:17:51 +0530

    Made the code into a sbt project

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "abcd"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
+
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.2.0"
---
+++ src/main/scala/XMLApp.scala
@@ -0,0 +1,7 @@
+import scala.xml.Elem
+object XMLApp extends App {
+  val person: Elem = <person>
+    <firstName>Mano</firstName>
+    <lastName>P</lastName>
+  </person>
+}
